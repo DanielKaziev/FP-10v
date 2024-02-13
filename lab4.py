9c78a9ce17b31d07631bd65d1c110da43654d3f5
Daniel <kaziev30@mail.ru>
2024-02-13 07:48:27 +0600

    add lab 4

---
+++ lab4.py
@@ -0,0 +1,45 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        merge_sort(L)
+        merge_sort(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+def sortWithStrategy(arr, strategy):
+    strategy(arr)
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+print(arr)
+sortWithStrategy(arr, bubble_sort)
+print(arr)
